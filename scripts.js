eb4fa4f9312d116b723a1d0253ae97770e7c7b35
RIDALOTFI <lotfirida1996@gmail.com>
2023-10-09 17:23:59 +0200

    versiónenterega

--- scripts.js
+++ scripts.js
@@ -1,1 +1,53 @@
 console.log(document.title);
+
+
+const lista = document.querySelector('.infocard')
+
+console.log(lista)
+
+const title_mod = document.getElementById('gen-1');
+
+title_mod.innerHTML='Generasión 1 Pokimon';
+
+
+
+
+
+const element =document.getElementsByTagName('div');
+
+element[2].style.background = 'red';
+
+
+const urlDoc = document.URL;
+
+console.log(urlDoc);
+
+const dominio = document.domain;
+
+console.log(dominio);
+
+const imgnodes=document.getElementsByTagName('img');
+
+console.log(imgnodes);
+
+
+
+const x = document.getElementsByTagName('img');
+    for (i = 0; i < x.length; i++) {
+        x[i].setAttribute("src", "https://media.giphy.com/media/2v170e71aanfi/giphy.gif");
+    }
+
+
+
+const y = document.querySelectorAll('.infocard-lg-data text-muted');
+    for (j = 0; j < y.length; j++) {
+        y[j].style.backgroundColor = 'green';
+    }
+
+
+const z = document.getElementsByClassName('.itype flying');
+    for (k = 0; k < z.length; k++) {
+        z[k].style.backgroundColor = 'blue';
+    }
+  
+
